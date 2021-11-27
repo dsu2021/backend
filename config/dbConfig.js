dfdce2797aed8fcd75af0a3768d72d6b8575b695
ongdv <ongdv94@gmail.com>
2021-11-27 15:09:27 +0900

    [UPDATE] User Update
    회원 CRUD 중, Create, Read
    
    Create => POST, INSERT Query
    
    Read => GET, SELECT Query
    
    Update => put, UPDATE Query,
    
    Delete => delete, DELETE Query

---
+++ config/dbConfig.js
@@ -0,0 +1,18 @@
+const {createPool} = require('mysql2/promise');
+// 운영환경 설정
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const dbInfo = {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+}
+
+console.log(dbInfo)
+
+const pool = createPool(dbInfo);
+
+module.exports = pool
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../config/dbConfig');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DSU2021 222 222' });
+  try {
+    pool.getConnection((err, conn) => {
+      if(err){
+        console.log(err);
+        throw err.message;
+      }
+      const query = "SELECT * FROM t_user;";
+      conn.query(query, (err, rows) => {
+        if(err){
+          console.log(err);
+          throw err.message;
+        }
+        conn.release();
+        res.status(200).json({status: 200, data: rows, message: 'Success'});  
+      });
+
+    })
+  } catch (error) {
+    res.status(500).json({status:500, message: error});
+  }
+  // res.render('index', { title: 'DSU2021 222 222' });
 });
 
 module.exports = router;
